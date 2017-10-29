acc4161aa2b509b0225d33d15416aa1d56afc7de
rsnoeber <robert.snoeberger@mathworks.com>
2017-10-28 22:53:33 -0400

    simplify Application.java

--- src/main/java/users/Application.java
+++ src/main/java/users/Application.java
@@ -1,30 +1,12 @@
 package users;
 
-import java.util.Arrays;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
-    private static final Logger log = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return (args) -> {
-            System.out.println("Starting");
-        };
-    }
-
 }
